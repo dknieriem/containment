0e1bff1373b0b4fa477850fa16ce5d51538092f0
Don Knieriem <donald.knieriem@gmail.com>
2014-08-27 00:41:27 -0500

    Initial commit of unity project

---
+++ Assets/MusicControllerInputHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicControllerInputHandler : MonoBehaviour
+{
+
+		public bool isPlaying = true;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+				if (Input.GetKeyDown (KeyCode.M)) {
+						isPlaying = !isPlaying;
+			
+			
+						AudioSource musicPlayer = GetComponent<AudioSource> ();
+			
+						if (isPlaying)
+								musicPlayer.Play ();
+						else
+								musicPlayer.Pause ();
+				}
+	
+		}
+}
