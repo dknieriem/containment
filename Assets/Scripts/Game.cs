25ad6073ebd9635da4650ce70d15c48bb6037de2
Don Knieriem <donald.knieriem@gmail.com>
2014-08-27 23:10:24 -0500

    Build 1! Music loops, main menu w/ exit button

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour
+{
+
+		public bool isDebug = true; //TODO on build, change to false
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+		
+		void ToggleDebug ()
+		{
+		
+				isDebug = !isDebug;
+				
+				Debug.Log ("Debug now: " + isDebug.ToString ());
+		
+		}
+		
+		void Exit ()
+		{
+		
+				Debug.Log ("Main Menu Exit");
+		
+				Application.Quit ();
+		
+		}
+}
---
+++ Assets/Scripts/MusicControllerInputHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicControllerInputHandler : MonoBehaviour
+{
+
+		public bool isPlaying = true;
+		
+		public AudioClip[] songs;
+		
+		public int currentClipNumber;
+		
+		private AudioSource musicPlayer;
+	
+		// Use this for initialization
+		void Start ()
+		{
+	
+				musicPlayer = GetComponent<AudioSource> ();
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+				if (Input.GetKeyDown (KeyCode.M)) {
+						isPlaying = !isPlaying;
+				}
+				
+				
+				
+	
+				if (!isPlaying)
+						musicPlayer.Stop ();
+		
+				if (isPlaying && (!musicPlayer.isPlaying || Input.GetButtonDown ("Next Song")))
+						PlayNextSong ();
+		}
+
+		void PlayNextSong ()
+		{
+			
+				currentClipNumber = (currentClipNumber + 1) % songs.Length;
+			
+				musicPlayer.clip = songs [currentClipNumber];
+			
+				musicPlayer.Play ();
+			
+		}
+
+}
