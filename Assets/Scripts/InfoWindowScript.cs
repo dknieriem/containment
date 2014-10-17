a33a613ba2b7ebd222b52fea0987e620189a66e9
somethingsilly <donald.knieriem@gmail.com>
2014-10-17 22:46:15 +0000

    InfoWindowScript.cs edited online with Bitbucket

--- Assets/Scripts/InfoWindowScript.cs
+++ Assets/Scripts/InfoWindowScript.cs
@@ -1,8 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class InfoWindowScript
+public class InfoWindowScript : MonoBehavior
 {
 
+public Text[] TextElements;
+
 //TODO: abstractify the infoscript classes to pull all child components of type ui.text, then have a method to implement called "update text"
+
+void Start()
+{
+TextElements = gameObject.GetComponents<Text>();
+}
+
+void Update()
+{
+//update each text element somehow?
+}
+
+
+void GetMessage(string ElementName, string newText)
+{
+
+}
+
 }
