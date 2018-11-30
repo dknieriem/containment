f37504613cd356d1e2687cf94d8b000cbdd1e676
Don <don@thecanarium.com>
2018-11-29 22:49:12 -0500

    UI tweak, minimap start
    working on implementing minimap. added a lot of sprite resources

---
+++ Assets/MapImager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapImager : MonoBehaviour {
+
+	//TODO: working on https://www.youtube.com/watch?time_continue=37&v=tijYJcJnl0I
+	public float SnapDelay = 1.0f;
+	public string SegmentName = "segment";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
