debf9954472d8c4fff45167525371edcac81b43f
Don <don@thecanarium.com>
2018-12-01 22:38:48 -0500

    minimap image creation

--- Assets/MapImager.cs
+++ Assets/MapImager.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MapImager : MonoBehaviour {
 
 	//TODO: working on https://www.youtube.com/watch?time_continue=37&v=tijYJcJnl0I
 	public float SnapDelay = 1.0f;
 	public string SegmentName = "segment";
+	public float Aspect = 1.0f;
+	public int SuperSize = 2;
+	public Camera otherCam;
 	// Use this for initialization
-	void Start () {
-		
+	public void Capture () {
+		Camera camera = GetComponent<Camera>();
+		camera.enabled = true;
+
+		Camera otherCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+		otherCam.enabled = false;
+
+		camera.aspect = Aspect;
+
+		//var xHalfUnit = camera.orthographicSize * Aspect;
+		//var yHalfUnit = camera.orthographicSize;
+
+		Helper.CreateAssetFolderIfNotExists("Minimap/Textures");
+
+		ScreenCapture.CaptureScreenshot("Assets/Minimap/Textures/map.png");
+
+		camera.enabled = false;
+		otherCam.enabled = true;
 	}
 	
 	// Update is called once per frame
